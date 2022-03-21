9a458300ebbb3274bb95d5414e2031e841b8a50e
ozgurutku <ozgurutku444@hotmail.com>
2022-03-21 23:18:44 +0300

    bug fix

--- RegexExample.java
+++ RegexExample.java
@@ -7,12 +7,12 @@ public class RegexExample {
 
     public static void main(String[] args){
 
-        final String findPattern = "(\\d+(\\.|\\,|\\\\,|\\/|\\-)\\d+(\\s||\\-)+t\\w*)|(\\d+(\\s||\\-)+t\\w*)";
-        final String removeTextPattern = "[^(\\d+(\\.|\\,|\\\\|\\/|\\-)\\d+)]";
+        final String findPattern = "(\\d+(\\.|\\,|\\\\,|\\/|\\-)\\d+(\\s||\\-)+t\\w*(\\.|\\,|\\\\,|\\/|\\-)+t\\w*)|(\\d+(\\s||\\-)+t\\w*)";
+        final String findNumberPattern = "((\\d+(\\.|\\,|\\\\|\\/|\\-)\\d+)|(\\d+))";
         final String commaPattern = "\\,";
         final String numberPattern = "\\d+";
 
-        String text = "1k 11-12 ton 14 tons k2 13 tons";
+        String text = "15/12 tons/tonne";
         //String text1 = "k1 15.14 tons k2 13 tons";
 
         Pattern p = Pattern.compile(findPattern);
@@ -20,26 +20,30 @@ public class RegexExample {
 
         if(m.find()){
             System.out.println("Text: "+m.group());
-            String onlyNumber = m.group().replaceAll(removeTextPattern,"");
-            System.out.println("Text removed: "+onlyNumber);
-            if (!(onlyNumber.contains(".") || onlyNumber.contains(","))){
-                System.out.println("Calculating the average of the numbers...");
-                double sum = 0;
-                double average = 0;
-                double size = 0;
-                p = Pattern.compile(numberPattern);
-                m = p.matcher(onlyNumber);
-                while (m.find()){
-                    sum = sum + Integer.parseInt(m.group());
-                    size ++;
+            Pattern p1 = Pattern.compile(findNumberPattern);
+            Matcher m1 = p1.matcher(m.group());
+            if (m1.find()){
+                String onlyNumber = m1.group();
+                System.out.println("Text removed: "+onlyNumber);
+                if (!(onlyNumber.contains(".") || onlyNumber.contains(",") || onlyNumber.matches(numberPattern))){
+                    System.out.println("Calculating the average of the numbers...");
+                    double sum = 0;
+                    double average = 0;
+                    double size = 0;
+                    p = Pattern.compile(numberPattern);
+                    m = p.matcher(onlyNumber);
+                    while (m.find()){
+                        sum = sum + Integer.parseInt(m.group());
+                        size ++;
+                    }
+                    average = sum / size;
+                    onlyNumber = String.valueOf(average);
                 }
-                average = sum / size;
-                onlyNumber = String.valueOf(average);
+                String numberReplaceComma = onlyNumber.replaceAll(commaPattern,".");
+                System.out.println("Number with dot: "+numberReplaceComma);
+                float f = Float.parseFloat(numberReplaceComma);
+                System.out.println("Convert to float: "+f);
             }
-            String numberReplaceComma = onlyNumber.replaceAll(commaPattern,".");
-            System.out.println("Number with dot: "+numberReplaceComma);
-            float f = Float.parseFloat(numberReplaceComma);
-            System.out.println("Convert to float: "+f);
         }
     }
 }
